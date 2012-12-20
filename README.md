e2db5a63928d1728e464fd83a4db4eefe27c3937
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-12-20 21:19:59 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-sauce-lab
-=========
+# sauce-lab
 
-Run tests on sauce labs
+  Run a single test file on sauce labs.  This just takes care of polling for the result.
+
+## Run(config)
+
+Config should have the following properties...
+
+Authenitcation:
+
+ - **user** - the user on sauce labs
+ - **key** - the key on sauce labs
+
+Test:
+
+ - **browser** - the exact browser & version to test on
+ - **url** - the url of the test page
+ - **code** - the code to execute on the page for each poll.
+ - **parse** - a function that gets called with the result of executing code, it should return null untill the test is complete, at which point it should return the result of the test.
+
+Logging:
+
+ - **name** - the name of the test
+ - **tags** - an array of tags for the test
---
+++ index.js
@@ -0,0 +1,39 @@
+var debug = require('debug')('sauce-lab:base');
+var wd = require('wd');
+var Q = require('q');
+
+module.exports = run;
+function run(config) {
+  var user = config.user;
+  var key = config.key;
+
+  var url = config.url;
+
+  var browser = config.browser;
+  browser.name = config.name;
+  browser.tags = config.tags;
+
+  var code = config.code;
+  var parse = config.parse;
+
+  var remote = wd.remote('ondemand.saucelabs.com', 80, user, key);
+  var init = Q.nfbind(remote.init.bind(remote));
+  var get = Q.nfbind(remote.get.bind(remote));
+  var exec = Q.nfbind(remote.eval.bind(remote));
+  function poll() {
+    exec(code)
+      .then(function (result) {
+        return parse(result);
+      })
+      .then(function (parsed) {
+        return parsed === null ? poll() : parsed;
+      });
+  }
+  return init(conf)
+    .then(function () {
+      return get(url);
+    })
+    .then(function () {
+      return poll();
+    });
+}
